fef12ec4b4fb4cf3b9ca5081a6f8dd51c7d72c18
Gert Hengeveld <info@ghengeveld.nl>
2011-11-13 14:26:35 +0100

    Fix sibling URL to support custom params set by other plugins

--- combilists/combilists.siblings.php
+++ combilists/combilists.siblings.php
@@ -22,28 +22,31 @@ if (in_array($parent, $cl_categories))
 		{
 			if ($cache->mem->exists('subcount_'.$sibling, 'combilists'))
 			{
-				$sub_count = $cache->mem->get('subcount_'.$sibling, 'combilists');
+				$sub_count = (int)$cache->mem->get('subcount_'.$sibling, 'combilists');
 			}
 		}
 		if($sub_count === null)
 		{
-			$sub_count = $db->query("SELECT SUM(structure_count) FROM $db_structure WHERE
+			$sub_count = (int)$db->query("SELECT SUM(structure_count) FROM $db_structure WHERE
 				structure_path LIKE '".$db->prep($structure['page'][$sibling]['rpath']).".%' OR
 				structure_path = ".$db->quote($structure['page'][$sibling]['rpath']))->fetchColumn();
 			$cache && $cache->mem && $cache->mem->store('subcount_'.$sibling, $sub_count, 'combilists');
 		}
 		$all_count += $sub_count;
+		$sibling_url_path = $list_url_path;
+		$sibling_url_path['c'] = $sibling;
 		$t->assign(array(
 			'LIST_SIBLING_CODE' => $sibling,
 			'LIST_SIBLING_TITLE' => $structure['page'][$sibling]['title'],
 			'LIST_SIBLING_DESC' => $structure['page'][$sibling]['desc'],
 			'LIST_SIBLING_ICON' => $structure['page'][$sibling]['icon'],
-			'LIST_SIBLING_URL' => cot_url('page', 'c='.$sibling),
+			'LIST_SIBLING_URL' => cot_url('page', $sibling_url_path),
 			'LIST_SIBLING_COUNT' => $sub_count
 		));
 		$t->parse('MAIN.LIST_SIBLINGS');
 	}
 	$t->assign('LIST_ALL_COUNT', $all_count);
 }
+
 
 ?>
