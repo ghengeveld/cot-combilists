5779e1601dc045a151e02a5b1e0e91bfa24a71c9
Gert Hengeveld <info@ghengeveld.nl>
2011-11-20 16:29:29 +0100

    Add support for listfilters.

--- combilists/combilists.siblings.php
+++ combilists/combilists.siblings.php
@@ -18,19 +18,46 @@ if (in_array($parent, $cl_categories))
 	foreach($siblings as $sibling)
 	{
 		$sub_count = null;
+		$cachename = 'subcount_'.$sibling;
+		if ($sqlfilters && $filterway)
+		{
+			$cachename .= '_' . md5(implode($filterway,$sqlfilters));
+		}
+		elseif ($o && $p)
+		{
+			if (!is_array($o)) $o = array($o);
+			if (!is_array($p)) $p = array($p);
+			$filters = array_combine($o, $p);
+			ksort($filters);
+			foreach ($filters as $key => $val)
+			{
+				$cachename .= "_$key=$val";
+			}
+		}
 		if($cache && $cache->mem)
 		{
-			if ($cache->mem->exists('subcount_'.$sibling, 'combilists'))
+			if ($cache->mem->exists($cachename, 'combilists'))
 			{
-				$sub_count = (int)$cache->mem->get('subcount_'.$sibling, 'combilists');
+				$sub_count = (int)$cache->mem->get($cachename, 'combilists');
 			}
 		}
 		if($sub_count === null)
 		{
-			$sub_count = (int)$db->query("SELECT SUM(structure_count) FROM $db_structure WHERE
-				structure_path LIKE '".$db->prep($structure['page'][$sibling]['rpath']).".%' OR
-				structure_path = ".$db->quote($structure['page'][$sibling]['rpath']))->fetchColumn();
-			$cache && $cache->mem && $cache->mem->store('subcount_'.$sibling, $sub_count, 'combilists');
+			$where = array();
+			$params = array();
+			$categories = implode("','", cot_structure_children('page', $sibling));
+			if ($categories)
+			{
+				$where['cat'] = "page_cat IN ('$categories')";
+			}
+			$where = ($where) ? 'WHERE ' . implode(' AND ', $where) : '';
+			if ($where && $sqlfilters && $filterway) $where .= ' AND (' . implode(" $filterway ", $sqlfilters) . ')';
+			if (is_array($sqlparams))
+			{
+				$params = array_merge($params, $sqlparams);
+			}
+			$sub_count = (int)$db->query("SELECT COUNT(*) FROM $db_pages $where", $params)->fetchColumn();
+			$cache && $cache->mem && $cache->mem->store($cachename, $sub_count, 'combilists');
 		}
 		$all_count += $sub_count;
 		$sibling_url_path = $list_url_path;
@@ -47,6 +74,5 @@ if (in_array($parent, $cl_categories))
 	}
 	$t->assign('LIST_ALL_COUNT', $all_count);
 }
-
 
 ?>
